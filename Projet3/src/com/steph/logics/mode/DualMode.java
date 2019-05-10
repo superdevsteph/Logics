f431226e0934475da84b3806ecd3be702f124264
Stéphanie <stephanie.mehraik@gmail.com>
2019-05-10 14:20:55 +0200

    Developpement du mode duel

--- Projet3/src/com/steph/logics/mode/DualMode.java
+++ Projet3/src/com/steph/logics/mode/DualMode.java
@@ -1,25 +1,58 @@
 package com.steph.logics.mode;
 
-public class DualMode extends Mode{
+import java.util.Scanner;
 
+import com.steph.logics.Utils;
+import com.steph.logics.game.Game;
+
+public class DualMode extends Mode {
+	private static String answer;
 	public DualMode() {
 		super("Dual");
 	}
 
 	@Override
 	public String getSCode() {
-		// TODO Auto-generated method stub
-		return null;
+
+		char secretCode[] = Utils.generate();
+		String sCode = String.valueOf(secretCode);
+
+		return sCode;
 	}
 
 	@Override
 	public String getAnswer() {
-		// TODO Auto-generated method stub
-		return null;
+		Scanner sc = new Scanner(System.in);
+		int turn = Game.getTurn();
+		if (turn %2 == 1)  {
+
+			System.out.println("entrez votre proposition");
+			sc.hasNextLine();
+			answer = sc.nextLine();
+
+		} else {
+char[] proposition = Utils.getProposition(answer);
+			
+
+			for (int i = 0; i < proposition.length; i++) {
+
+				switch (Game.result[i]) {
+
+				case '-':
+					proposition[i] --;
+					break;
+				case '+':
+					proposition[i] ++;
+					break;
+				default:
+					proposition[i] = proposition[i];
+				}
+			}
+			answer = new String(proposition);
+			}
+		
+		return answer;
+
 	}
-
-
-
-
 
 }
