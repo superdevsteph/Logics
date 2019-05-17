c1073a1c94b9396bb59feeabe9c3456aaecfe702
Stéphanie <stephanie.mehraik@gmail.com>
2019-05-17 11:17:03 +0200

    Ajout des fichiers de configuration

---
+++ Projet3/src/com/steph/logics/App.java
@@ -0,0 +1,37 @@
+package com.steph.logics;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class App {
+
+   /**
+    * Déclaration du logger que nous allons utilisé
+    * On utilise la classe App pour générer un nom de logger
+    * unique dans notre application ici:
+    * com.steph.logics.App
+    */
+   private static Logger logger = Logger.getLogger(App.class);
+
+   /**
+    * Une méthode main où on va juste effectuer un log
+    * Un Hello World Log4j en somme
+    */
+   public static void main(String[] args){
+	   
+	   PropertyConfigurator.configure(
+		          App.class.getClassLoader().getResource("log4j.properties"));
+
+      logger.info("Hello World!");
+      try {
+    	    // équivaut à logger.info("Message d'information");
+    	    logger.log(Level.INFO, "Message d'information");
+    		// Code pouvant soulever une Exception
+    		//...
+    	} catch(Exception e) {
+    	    // équivaut à logger.log(Level.FATAL, "Une exception est survenue", e);
+    	    logger.fatal("Une exception est survenue", e);
+    	}
+   }
+
+}
